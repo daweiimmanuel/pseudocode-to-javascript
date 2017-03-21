fdc455730d446e94d3f4794416774aaa66c7813c
David Immanuel <daweiimmanuel@users.noreply.github.com>
2017-03-21 17:38:13 +0700

    Add case1.js

---
+++ case1.js
@@ -0,0 +1,7 @@
+var nama = prompt('Masukkan nama','');
+var pembilang = prompt('Masukkan angka pembilang','');
+var penyebut = prompt('Masukkan angka penyebut','');
+
+var hasil = pembilang/penyebut;
+
+console.log('Halo '+nama+', '+pembilang+' dibagi '+penyebut+' adalah sama dengan '+hasil);
